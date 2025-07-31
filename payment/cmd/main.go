d8279064363422ca10dce2f2a015fc6d211028aa
Dima <lastfm4321@gmail.com>
2025-07-31 15:50:07 +0300

    di for payment

--- payment/cmd/main.go
+++ payment/cmd/main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"net"
-	"os"
+	"github.com/dfg007star/go_rocket/payment/internal/app"
+	"github.com/dfg007star/go_rocket/platform/pkg/closer"
+	"github.com/dfg007star/go_rocket/platform/pkg/logger"
+	"go.uber.org/zap"
 	"os/signal"
 	"syscall"
+	"time"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-
-	paymentAPI "github.com/dfg007star/go_rocket/payment/internal/api/payment/v1"
 	"github.com/dfg007star/go_rocket/payment/internal/config"
-	paymentRepository "github.com/dfg007star/go_rocket/payment/internal/repository/payment"
-	paymentService "github.com/dfg007star/go_rocket/payment/internal/service/payment"
-	paymentV1 "github.com/dfg007star/go_rocket/shared/pkg/proto/payment/v1"
 )
 
 const configPath = "../deploy/compose/payment/.env"
@@ -26,44 +22,30 @@ func main() {
 		panic(fmt.Errorf("error loading config: %w", err))
 	}
 
-	lis, err := net.Listen("tcp", config.AppConfig().PaymentGRPC.Address())
+	appCtx, appCancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer appCancel()
+	defer gracefulShutdown()
+
+	closer.Configure(syscall.SIGINT, syscall.SIGTERM)
+
+	a, err := app.New(appCtx)
 	if err != nil {
-		log.Printf("failed to listen: %v\n", err)
+		logger.Error(appCtx, "❌ Не удалось создать приложение", zap.Error(err))
 		return
 	}
 
-	defer func() {
-		if err := lis.Close(); err != nil {
-			log.Printf("failed to close listener: %v\n", err)
-		}
-	}()
+	err = a.Run(appCtx)
+	if err != nil {
+		logger.Error(appCtx, "❌ Ошибка при работе приложения", zap.Error(err))
+		return
+	}
+}
 
-	// Создаем gRPC сервер
-	s := grpc.NewServer()
+func gracefulShutdown() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	// Регистрируем наш сервис
-	repo := paymentRepository.NewRepository()
-	service := paymentService.NewService(repo)
-	api := paymentAPI.NewAPI(service)
-	paymentV1.RegisterPaymentServiceServer(s, api)
-
-	// Включаем рефлексию для отладки
-	reflection.Register(s)
-
-	go func() {
-		log.Printf("🚀 gRPC server listening on %v\n", config.AppConfig().PaymentGRPC.Address())
-		err = s.Serve(lis)
-		if err != nil {
-			log.Printf("failed to serve: %v\n", err)
-			return
-		}
-	}()
-
-	// Graceful shutdown
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("🛑 Shutting down gRPC server...")
-	s.GracefulStop()
-	log.Println("✅ Server stopped")
+	if err := closer.CloseAll(ctx); err != nil {
+		logger.Error(ctx, "❌ Ошибка при завершении работы", zap.Error(err))
+	}
 }
---
+++ payment/internal/app/di.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"context"
+	paymentAPI "github.com/dfg007star/go_rocket/payment/internal/api/payment/v1"
+	"github.com/dfg007star/go_rocket/payment/internal/repository"
+	paymentRepository "github.com/dfg007star/go_rocket/payment/internal/repository/payment"
+	"github.com/dfg007star/go_rocket/payment/internal/service"
+	paymentService "github.com/dfg007star/go_rocket/payment/internal/service/payment"
+	paymentV1 "github.com/dfg007star/go_rocket/shared/pkg/proto/payment/v1"
+)
+
+type diContainer struct {
+	paymentV1API paymentV1.PaymentServiceServer
+
+	paymentService service.PaymentService
+
+	paymentRepository repository.PaymentRepository
+}
+
+func NewDiContainer() *diContainer {
+	return &diContainer{}
+}
+
+func (d *diContainer) PaymentV1API(ctx context.Context) paymentV1.PaymentServiceServer {
+	if d.paymentV1API == nil {
+		d.paymentV1API = paymentAPI.NewAPI(d.PaymentService(ctx))
+	}
+
+	return d.paymentV1API
+}
+
+func (d *diContainer) PaymentService(ctx context.Context) service.PaymentService {
+	if d.paymentService == nil {
+		d.paymentService = paymentService.NewService(d.PaymentRepository(ctx))
+	}
+
+	return d.paymentService
+}
+
+func (d *diContainer) PaymentRepository(ctx context.Context) repository.PaymentRepository {
+	if d.paymentRepository == nil {
+		d.paymentRepository = paymentRepository.NewRepository()
+	}
+
+	return d.paymentRepository
+}
